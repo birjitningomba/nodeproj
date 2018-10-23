93c5fb5de019e4511bfea8f08c03bb28a3ec403b
B Ningomba <birjitbbm@gmail.com>
2018-10-23 21:08:18 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 const config = require('./db');
 const FeedbackRoute = require('./feedbackroute');
